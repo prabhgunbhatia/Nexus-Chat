77c79d8bace6af7acd718c8d72116c2e3141f5ba
Prabh2006 <110138670+Prabh2006@users.noreply.github.com>
2025-01-15 15:44:59 -0500

    added templates and css

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
 
-    
